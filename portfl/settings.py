fa3b09183a0ead9440d7832f8476bf536b2ad8a9
Ortho Cgm <orthocgm@gmail.com>
2026-02-23 18:31:00 +0100

    Fix staticfiles storage for Render collectstatic

--- portfl/settings.py
+++ portfl/settings.py
@@ -151,6 +151,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'portfl_app', 'static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
