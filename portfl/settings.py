2537115fa67ff6a94571780a2597e8d060362002
Ortho Cgm <orthocgm@gmail.com>
2026-02-23 19:04:30 +0100

    Fix production settings (env vars, security)

--- portfl/settings.py
+++ portfl/settings.py
@@ -81,7 +81,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'portfl_app.middleware.VisitorTrackingMiddleware',
+    #'portfl_app.middleware.VisitorTrackingMiddleware',
 ]
 
 ROOT_URLCONF = 'portfl.urls'
