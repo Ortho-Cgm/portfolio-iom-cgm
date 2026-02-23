091b1c248908327f3fe20771e888ee178ba262a2
Ortho Cgm <orthocgm@gmail.com>
2026-02-23 18:15:01 +0100

    Prepare Django app for Render (env vars, gunicorn, Procfile)

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn portfl.wsgi:application
--- portfl/settings.py
+++ portfl/settings.py
@@ -24,12 +24,15 @@ TEMPLATES_DIRS = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lr83ck(l@lb*f+d02b!vz(by(_m=k3tqape)lgxqops6jtnl!('
+#SECRET_KEY = 'django-insecure-lr83ck(l@lb*f+d02b!vz(by(_m=k3tqape)lgxqops6jtnl!('
+
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
@@ -37,8 +40,11 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = "orthocgm@gmail.com"
-EMAIL_HOST_PASSWORD = "brugaghkudvepzog"
+#EMAIL_HOST_USER = "orthocgm@gmail.com"
+#EMAIL_HOST_PASSWORD = "brugaghkudvepzog"
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = "Portfolio <orthocgm@gmail.com>"
 
