de328d44006fb0b78bdd2c4b30de7a84259bb2bd
Ortho Cgm <orthocgm@gmail.com>
2026-02-24 02:36:32 +0100

    Configure PostgreSQL for Render

--- portfl/settings.py
+++ portfl/settings.py
@@ -116,23 +116,13 @@ WSGI_APPLICATION = 'portfl.wsgi.application'
     }
 }"""
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-if DATABASE_URL:
-    DATABASES = {
-        "default": dj_database_url.config(
-            default=DATABASE_URL,
-            conn_max_age=600,
-            ssl_require=True,
-        )
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ["DATABASE_URL"],
+        conn_max_age=600,
+        ssl_require=True,
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
