b928f8de3c6d2b6e795a864cf02f0af975c04b4a
Ortho Cgm <orthocgm@gmail.com>
2026-02-24 04:25:15 +0100

    Fix Cloudinary configuration for media files

--- portfl/settings.py
+++ portfl/settings.py
@@ -68,15 +68,15 @@ cloudinary.config(
 # Application definition
 
 INSTALLED_APPS = [
-    "cloudinary",
-    "cloudinary_storage",
-
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "cloudinary",
+    "cloudinary_storage",
 
     "portfl_app",
 ]
